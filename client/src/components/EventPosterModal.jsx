87f0b42a0f51a30407b95e87c9532aab230e039b
Yash Chichad <yashchichad04@gmail.com>
2026-08-02 02:16:10 +0530

    Update EventPosterModal to use vertical poster on mobile

--- client/src/components/EventPosterModal.jsx
+++ client/src/components/EventPosterModal.jsx
@@ -37,7 +37,7 @@ export default function EventPosterModal() {
     setIsOpen(false);
   };
 
-  const imageSrc = "/competition_poster.png";
+  const imageSrc = isMobile ? "/competition-poster-mobile.png" : "/competition_poster.png";
 
   return (
     <div 
